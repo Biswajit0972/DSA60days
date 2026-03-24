0bb6f6e31eff92d73277262a2fdebbd95ef11216
Biswajit0981 <codingpro0981@gmail.com>
2026-03-24 14:54:54 +0530

    Count digit from  a number

--- src/Day2/Solutions.java
+++ src/Day2/Solutions.java
@@ -1,4 +1,12 @@
 package Day2;
 
 public class Solutions {
+    public int countDigits(int n) {
+        return Long.toString(n).length();
+    }
+
+    public static void main(String[] args) {
+        Solutions s = new Solutions();
+        System.out.println(s.countDigits(23));
+    }
 }
