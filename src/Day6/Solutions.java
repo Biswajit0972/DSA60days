499e1b0125379924097734d5d66394169a3623b4
Biswajit0981 <codingpro0981@gmail.com>
2026-03-28 13:03:44 +0530

    rotate by kth place

---
+++ src/Day6/Solutions.java
@@ -0,0 +1,31 @@
+package Day6;
+
+import java.util.Arrays;
+
+public class Solutions {
+    public void rotate(int[] nums, int k) {
+        if (k == 0) return;
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, k, nums.length-1);
+        reverse(nums,0, k-1);
+    }
+
+    public void reverse(int [] nums, int st,  int ed) {
+        if (st >= ed) {
+            return;
+        }
+
+        int temp = nums[st];
+        nums[st] = nums[ed];
+        nums[ed] = temp;
+        reverse(nums,  st+1, ed-1);
+    }
+
+
+    public static void main(String[] args) {
+        Solutions s = new Solutions();
+        int [] nums = {-1,-100,3,99};
+        s.rotate(nums, 2);
+        System.out.println(Arrays.toString(nums));
+    }
+}
