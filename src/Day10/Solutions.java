67dab4604938203e6907a7b723fd73e7167d2761
Biswajit0981 <codingpro0981@gmail.com>
2026-04-02 08:42:20 +0530

    Day10: leaders

--- src/Day10/Solutions.java
+++ src/Day10/Solutions.java
@@ -1,5 +1,7 @@
 package Day10;
 
+import java.util.ArrayList;
+
 public class Solutions {
     public int maxProfit(int[] prices) {
         int maxProfit =  0;
@@ -14,6 +16,24 @@ public class Solutions {
         // ! remark : 10^4 so it become 10^6 not good for all test cases. 10^8  took 1 sec to execute.
 
         return maxProfit;
+    }
+
+    public ArrayList<Integer> leaders(int arr[]) {
+        ArrayList<Integer> res = new ArrayList<>();
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            boolean isLeader = true;
+            for (int j = i+1; j < n; j ++) {
+                if (arr[i] < arr[j]) {
+                    isLeader = false;
+                    break;
+                }
+            }
+
+            if (isLeader) res.add(arr[i]);
+        }
+
+        return res;
     }
 
     public static void main(String[] args) {
