19d752ef2c63ca8f9d488a6fbe67ba7ff475d8bd
Biswajit0981 <codingpro0981@gmail.com>
2026-04-02 08:04:58 +0530

    Day10: 2149. Rearrange Array Elements by Sign

--- src/Day10/Optimize.java
+++ src/Day10/Optimize.java
@@ -15,4 +15,22 @@ public class Optimize {
 
         return maxProfit;
     }
+
+    public int[] rearrangeArray(int[] nums) {
+        int pos= 0;
+        int neg = 1;
+        int [] res = new int[nums.length];
+
+        for (int num :  nums) {
+            if (num > 0) {
+                res[pos] = num;
+                pos+=2;
+            }else {
+                res[neg] = num;
+                neg+= 2;
+            }
+        }
+
+        return res;
+    }
 }
