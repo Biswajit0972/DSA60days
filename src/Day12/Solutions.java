6a7fb1e527429d86652bcf76a218980199f350d0
Biswajit0981 <codingpro0981@gmail.com>
2026-04-06 19:16:49 +0530

    Day12: pascale

--- src/Day12/Solutions.java
+++ src/Day12/Solutions.java
@@ -1,11 +1,14 @@
 package Day12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solutions {
     public int subarraySum(int[] nums, int k) {
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
             int sum = 0;
-            for (int j = i; j  < nums.length; j++) {
+            for (int j = i; j < nums.length; j++) {
                 sum += nums[j];
                 if (sum == k) {
                     count++;
@@ -16,7 +19,50 @@ public class Solutions {
         return count;
     }
 
-    public static void main(String[] args) {
+    public List<Integer> majorityElement(int[] nums) {
+        int count1 = 0, count2 = 0;
+        int candidate1 = 0, candidate2 = 0;
 
+        // Step 1: Find candidates
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Step 2: Verify candidates
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+
+        List<Integer> res = new ArrayList<>();
+        int n = nums.length;
+
+        if (count1 > n / 3) res.add(candidate1);
+        if (count2 > n / 3) res.add(candidate2);
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solutions s = new Solutions();
+        for (Integer num : s.majorityElement(new int [] {1,2})) {
+            System.out.print(num);
+        }
     }
 }
