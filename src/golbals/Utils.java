278eeacf7e8079b8525103a289a61f289938ff25
Biswajit0981 <codingpro0981@gmail.com>
2026-04-16 08:12:54 +0530

    Day13: forgot to  commit

---
+++ src/golbals/Utils.java
@@ -0,0 +1,26 @@
+package golbals;
+
+import java.util.List;
+
+public class Utils {
+    public static <T> void print2dList(List<List<T>> list) {
+        System.out.print("[");
+        for (List<T> list1 : list) {
+            System.out.print("[");
+            for (T num : list1) {
+                System.out.print(num + ",");
+            }
+            System.out.print("]");
+
+        }
+        System.out.println("]");
+    }
+
+    public static <T> void printList(List<T> nums) {
+        System.out.println("[");
+        for (T num : nums) {
+            System.out.print(num + ", ");
+        }
+        System.out.println("]");
+    }
+}
